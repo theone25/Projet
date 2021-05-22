ab7f58c50ef5386945e0573abc5bd478792cba3e
Mohamed Bouziani <mohamedbouziani25@gmail.com>
2021-05-22 15:03:22 +0100

    update dependencies

--- app/src/main/java/com/mine/projet/Main2Activity.java
+++ app/src/main/java/com/mine/projet/Main2Activity.java
@@ -1,13 +1,23 @@
 package com.mine.projet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
+import com.mine.projet.fragments.ImageListFragment;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.core.view.GravityCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,38 +25,59 @@ import androidx.navigation.ui.NavigationUI;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
-public class Main2Activity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
-    private AppBarConfiguration mAppBarConfiguration;
+public class Main2Activity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public static int notificationCountCart = 0;
+    static ViewPager viewPager;
+    static TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        if (viewPager != null) {
+            setupViewPager(viewPager);
+            tabLayout.setupWithViewPager(viewPager);
+        }
+
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        invalidateOptionsMenu();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -55,9 +86,144 @@ public class Main2Activity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        // Get the notifications MenuItem and
+        // its LayerDrawable (layer-list)
+        MenuItem item = menu.findItem(R.id.action_cart);
+        //NotificationCountSetClass.setAddToCart(Main2Activity.this, item,notificationCountCart);
+        // force the ActionBar to relayout its MenuItems.
+        // onCreateOptionsMenu(Menu) will be called again.
+        invalidateOptionsMenu();
+        return super.onPrepareOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_search) {
+            //startActivity(new Intent(Main2Activity.this, SearchResultActivity.class));
+            return true;
+        }else if (id == R.id.action_cart) {
+
+           /* NotificationCountSetClass.setAddToCart(Main2Activity.this, item, notificationCount);
+            invalidateOptionsMenu();*/
+            //startActivity(new Intent(Main2Activity.this, CartListActivity.class));
+
+           /* notificationCount=0;//clear notification count
+            invalidateOptionsMenu();*/
+            return true;
+        }else {
+            //startActivity(new Intent(Main2Activity.this, EmptyActivity.class));
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        Adapter adapter = new Adapter(getSupportFragmentManager());
+        ImageListFragment fragment = new ImageListFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("type", 1);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_1));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 2);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_2));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 3);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_3));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 4);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_4));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 5);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_5));
+        fragment = new ImageListFragment();
+        bundle = new Bundle();
+        bundle.putInt("type", 6);
+        fragment.setArguments(bundle);
+        adapter.addFragment(fragment, getString(R.string.item_6));
+        viewPager.setAdapter(adapter);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_item1) {
+            viewPager.setCurrentItem(0);
+        } else if (id == R.id.nav_item2) {
+            viewPager.setCurrentItem(1);
+        } else if (id == R.id.nav_item3) {
+            viewPager.setCurrentItem(2);
+        } else if (id == R.id.nav_item4) {
+            viewPager.setCurrentItem(3);
+        } else if (id == R.id.nav_item5) {
+            viewPager.setCurrentItem(4);
+        }else if (id == R.id.nav_item6) {
+            viewPager.setCurrentItem(5);
+        }else if (id == R.id.my_wishlist) {
+            //startActivity(new Intent(Main2Activity.this, WishlistActivity.class));
+        }else if (id == R.id.my_cart) {
+            //startActivity(new Intent(Main2Activity.this, CartListActivity.class));
+        }else if(id == R.id.my_orders){
+            //startActivity(new Intent(Main2Activity.this, MyOrderActivity.class));
+        }else if(id == R.id.my_account){
+            //startActivity(new Intent(Main2Activity.this, AccountActivity.class));
+        }
+        else {
+            //startActivity(new Intent(Main2Activity.this, EmptyActivity.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+
+    static class Adapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragments = new ArrayList<>();
+        private final List<String> mFragmentTitles = new ArrayList<>();
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragments.add(fragment);
+            mFragmentTitles.add(title);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitles.get(position);
+        }
+    }
+
 }
---
+++ app/src/main/java/com/mine/projet/fragments/ImageListFragment.java
@@ -0,0 +1,157 @@
+package com.mine.projet.fragments;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import com.mine.projet.Main2Activity;
+import com.mine.projet.R;
+import com.mine.projet.imgUtils;
+
+public class ImageListFragment  extends Fragment {
+    public static final String STRING_IMAGE_URI = "ImageUri";
+    public static final String STRING_IMAGE_POSITION = "ImagePosition";
+    private static Main2Activity mActivity;
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = (Main2Activity) getActivity();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        RecyclerView rv = (RecyclerView) inflater.inflate(R.layout.layout_rv_list, container, false);
+        setupRecyclerView(rv);
+        return rv;
+    }
+
+    private void setupRecyclerView(RecyclerView recyclerView) {
+      /*  if (ImageListFragment.this.getArguments().getInt("type") == 1) {
+            recyclerView.setLayoutManager(new LinearLayoutManager(recyclerView.getContext()));
+        } else if (ImageListFragment.this.getArguments().getInt("type") == 2) {
+            StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
+        } else {
+            GridLayoutManager layoutManager = new GridLayoutManager(recyclerView.getContext(), 3);
+            layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+            recyclerView.setLayoutManager(layoutManager);
+        }*/
+        String[] items=null;
+        if (ImageListFragment.this.getArguments().getInt("type") == 1){
+            items = imgUtils.getOffersUrls();
+        }else if (ImageListFragment.this.getArguments().getInt("type") == 2){
+            items =imgUtils.getElectronicsUrls();
+        }else if (ImageListFragment.this.getArguments().getInt("type") == 3){
+            items =imgUtils.getLifeStyleUrls();
+        }else if (ImageListFragment.this.getArguments().getInt("type") == 4){
+            items =imgUtils.getHomeApplianceUrls();
+        }else if (ImageListFragment.this.getArguments().getInt("type") == 5){
+            items =imgUtils.getBooksUrls();
+        }else {
+            items = imgUtils.getImageUrls();
+        }
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(new SimpleStringRecyclerViewAdapter(recyclerView, items));
+    }
+
+    public static class SimpleStringRecyclerViewAdapter
+            extends RecyclerView.Adapter<SimpleStringRecyclerViewAdapter.ViewHolder> {
+
+        private String[] mValues;
+        private RecyclerView mRecyclerView;
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+            public final View mView;
+            public final SimpleDraweeView mImageView;
+            public final LinearLayout mLayoutItem;
+            public final ImageView mImageViewWishlist;
+
+            public ViewHolder(View view) {
+                super(view);
+                mView = view;
+                mImageView = (SimpleDraweeView) view.findViewById(R.id.image1);
+                mLayoutItem = (LinearLayout) view.findViewById(R.id.layout_item);
+                mImageViewWishlist = (ImageView) view.findViewById(R.id.ic_wishlist);
+            }
+        }
+
+        public SimpleStringRecyclerViewAdapter(RecyclerView recyclerView, String[] items) {
+            mValues = items;
+            mRecyclerView = recyclerView;
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+            return new ViewHolder(view);
+        }
+
+        @Override
+        public void onViewRecycled(ViewHolder holder) {
+            if (holder.mImageView.getController() != null) {
+                holder.mImageView.getController().onDetach();
+            }
+            if (holder.mImageView.getTopLevelDrawable() != null) {
+                holder.mImageView.getTopLevelDrawable().setCallback(null);
+//                ((BitmapDrawable) holder.mImageView.getTopLevelDrawable()).getBitmap().recycle();
+            }
+        }
+
+        @Override
+        public void onBindViewHolder(final ViewHolder holder, final int position) {
+           /* FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) holder.mImageView.getLayoutParams();
+            if (mRecyclerView.getLayoutManager() instanceof GridLayoutManager) {
+                layoutParams.height = 200;
+            } else if (mRecyclerView.getLayoutManager() instanceof StaggeredGridLayoutManager) {
+                layoutParams.height = 600;
+            } else {
+                layoutParams.height = 800;
+            }*/
+            final Uri uri = Uri.parse(mValues[position]);
+            holder.mImageView.setImageURI(uri);
+            holder.mLayoutItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /*Intent intent = new Intent(mActivity, ItemDetailsActivity.class);
+                    intent.putExtra(STRING_IMAGE_URI, mValues[position]);
+                    intent.putExtra(STRING_IMAGE_POSITION, position);
+                    mActivity.startActivity(intent);*/
+
+                }
+            });
+
+            //Set click action for wishlist
+            holder.mImageViewWishlist.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    imgUtils images = new imgUtils();
+                    images.addWishlistImageUri(mValues[position]);
+                    holder.mImageViewWishlist.setImageResource(R.drawable.ic_favorite_black_18dp);
+                    notifyDataSetChanged();
+                    Toast.makeText(mActivity,"Produit Ajouté.",Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mValues.length;
+        }
+    }
+}
